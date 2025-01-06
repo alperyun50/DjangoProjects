2bdd127021b332dd21b95358251b67d39e93ed21
alperyun50 <yavuzun50@hotmail.com>
2025-01-06 18:49:03 +0300

    posts urls and views fixed

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
+# def homepage(request):
+#     return HttpResponse("hello world! I'm home. ")
 def homepage(request):
-    return HttpResponse("hello world! I'm home. ")
+    return render(request, 'home.html')
 
+# def about(request):
+#     return HttpResponse("my about page")
 def about(request):
-    return HttpResponse("my about page")
+    return render(request, 'about.html')
